245bf861792a43b3dac9f05db9a86b4e9e57e984
刘航 <hang.liu6@amh-group.com>
2022-08-29 17:47:36 +0800

    新增hello接口用于测试

--- src/main/java/com/liuhangs/learning/wechatminiprogram/config/WechatConfig.java
+++ src/main/java/com/liuhangs/learning/wechatminiprogram/config/WechatConfig.java
@@ -33,4 +33,6 @@ public class WechatConfig {
         template.afterPropertiesSet();
         return template;
     }
+
+
 }
--- src/main/java/com/liuhangs/learning/wechatminiprogram/controller/UserController.java
+++ src/main/java/com/liuhangs/learning/wechatminiprogram/controller/UserController.java
@@ -3,6 +3,8 @@ package com.liuhangs.learning.wechatminiprogram.controller;
 import com.liuhangs.learning.wechatminiprogram.common.Result;
 import com.liuhangs.learning.wechatminiprogram.service.UserService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +20,10 @@ public class UserController {
     public Result<String> createSess8ion(String authCode) {
         String session = userService.createSession(authCode);
         return Result.success(session);
+    }
+
+    @GetMapping("/hello")
+    public Result<String> hello() {
+        return Result.success("hello world");
     }
 }
