6df45494bbca2d125e174dfdcc0b889edf60d5fb
刘航 <hang.liu6@amh-group.com>
2022-08-31 18:54:22 +0800

    接入nacos

--- src/main/java/com/liuhangs/learning/wechatminiprogram/service/UserService.java
+++ src/main/java/com/liuhangs/learning/wechatminiprogram/service/UserService.java
@@ -1,35 +1,39 @@
 package com.liuhangs.learning.wechatminiprogram.service;
 
-import com.liuhangs.learning.wechatminiprogram.common.Constants;
-import com.liuhangs.learning.wechatminiprogram.common.ErrorCode;
-import com.liuhangs.learning.wechatminiprogram.dto.AuthCodeDTO;
-import com.liuhangs.learning.wechatminiprogram.exception.BusinessException;
-import com.liuhangs.learning.wechatminiprogram.utils.HttpUtils;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Service;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import com.liuhangs.learning.wechatminiprogram.common.Constants;
+import com.liuhangs.learning.wechatminiprogram.common.ErrorCode;
+import com.liuhangs.learning.wechatminiprogram.dto.AuthCodeDTO;
+import com.liuhangs.learning.wechatminiprogram.exception.BusinessException;
+import com.liuhangs.learning.wechatminiprogram.utils.HttpUtils;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@RefreshScope
 public class UserService {
 
     private static final String AUTH_CODE_URL = "https://api.weixin.qq.com/sns/jscode2session";
 
-    @Value("${miniprogram.app.id}")
+    @Value(value = "${miniprogram.app.id}")
     private String APP_ID;
 
-    @Value("${miniprogram.app.secret}")
+    @Value(value = "${miniprogram.app.secret}")
     private String APP_SECRET;
 
     private final RedisTemplate<String, Object> redisTemplate;
